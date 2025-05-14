8406b140787b349c7e8377381fa9876eda1de464
Umma Salma Akter Joty <163902994+ummasalmaakterjoty@users.noreply.github.com>
2025-05-15 01:31:01 +0600

    settings.py

--- CORE/settings.py
+++ CORE/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GEMINI_API_KEY = 'AIzaSyDySDirj3E_bQEegqVzQ_wu0KlloATohIc'
